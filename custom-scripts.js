7f1ae42c40f063e01caabf48391c2242caee31cd
Blimeys <djeanmath@gmail.com>
2017-04-28 22:31:45 +0100

    making changes to menu

---
+++ custom-scripts.js
@@ -0,0 +1,11 @@
+function changeImage() {
+
+    if (document.getElementById("toggleImg").src == "menu.svg")
+    {
+        document.getElementById("toggleImg").src = "cross.svg";
+    }
+    else
+    {
+        document.getElementById("toggleImg").src = "menu.svg";
+    }
+}

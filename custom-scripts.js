6ab2d61ad380cbbedac5bb651e1e54141456f8d2
Blimeys <djeanmath@gmail.com>
2017-05-01 17:00:33 +0100

    buggy menu - solving

--- custom-scripts.js
+++ custom-scripts.js
@@ -1,11 +1,11 @@
 function changeImage() {
-
-    if (document.getElementById("toggleImg").src == "menu.svg")
+  console.log("clicked");
+    if (document.getElementById('#toggleImg').src == "menu.svg")
     {
-        document.getElementById("toggleImg").src = "cross.svg";
+        document.getElementById('#toggleImg').src = "cross.svg";
     }
     else
     {
-        document.getElementById("toggleImg").src = "menu.svg";
+        document.getElementById('#toggleImg').src = "menu.svg";
     }
 }

ccf1d74a4600448795856c8b52c66be9dedcd608
Blimeys <djeanmath@gmail.com>
2017-05-07 14:55:53 +0100

    adding more pages

--- custom-scripts.js
+++ custom-scripts.js
@@ -7,3 +7,21 @@ function changeMenuIcon() {
     $('#toggleImg').attr("src","menu.svg");
   }
 }
+function initSlider(){
+$(".single-item").slick({
+	dots: true,
+  speed: 300,
+  // customPaging : function(slider, i) {
+  //         return '<a href="#"><img src="slide-dot.png" /><img src="slide-dot-active.png" /></a>';
+  //     },
+  arrows: true
+});
+}
+
+
+
+$(document).on('ready', function () {
+    initSlider();
+    sliderArrows();
+    console.log("slider loaded");
+});
